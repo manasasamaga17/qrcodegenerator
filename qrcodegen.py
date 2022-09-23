427c83ba357eb6b10ec763d370628532f91fc912
Manasa Samaga <93869716+manasasamaga17@users.noreply.github.com>
2022-09-23 21:11:38 +0530

    Update qrcodegen.py

--- qrcodegen.py
+++ qrcodegen.py
@@ -1,1 +1,8 @@
 
+import qrcode  qr=qrcode.QRCode(     version=15,#version of the qrcode, higher the number bigger the qr code image and complicated picture     
+box_size=10,#size of the box in which qr code will be displayed     
+border=5 #it is the white part of image, border in all 4 sides with white color ) 
+qr.add_data("data") 
+qr.make(fit=True) 
+img=qr.make_image(fill="black", back_color="white") 
+img.save("test.png")
